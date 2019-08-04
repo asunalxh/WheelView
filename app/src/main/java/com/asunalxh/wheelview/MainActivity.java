787a5d81aece6dd85746fb57added4c14263e96f
asunalxh <asunalxh@163.com>
2019-08-05 01:33:03 +0800

    update

--- app/src/main/java/com/asunalxh/wheelview/MainActivity.java
+++ app/src/main/java/com/asunalxh/wheelview/MainActivity.java
@@ -11,30 +11,9 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Handler handler=new Handler();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        Button button=findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                final CourseSelectorHelper courseSelectorHelper=new CourseSelectorHelper(MainActivity.this);
-                BaseDialog.Builder builder=new BaseDialog.Builder(MainActivity.this,courseSelectorHelper.getView());
-                builder.setContentHeight(600);
-                builder.setContentWidth(750);
-                builder.setRightButtonOnClickListener(new BaseDialog.Builder.onButtonClickListener() {
-                    @Override
-                    public void onClick() {
-                        Toast.makeText(getBaseContext(),courseSelectorHelper.getDayOfWeek()+" "+courseSelectorHelper.getStartCourse()+" "+courseSelectorHelper.getEndCourse(),Toast.LENGTH_SHORT).show();
-                    }
-                });
-                BaseDialog dialog=builder.create();
-                dialog.show();
-            }
-        });
-
     }
 }
